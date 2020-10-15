875359dcfd7a010abf4016d117378ace12a0b628
Thomas Panitz <github@thomas-panitz.de>
2020-10-15 23:49:17 +0200

    delete color from Taskcreation
    Signed-off-by: Thomas Panitz <github@thomas-panitz.de>

--- src/main/java/com/twoyang/prodactivity/server/api/TaskCreation.java
+++ src/main/java/com/twoyang/prodactivity/server/api/TaskCreation.java
@@ -1,14 +1,13 @@
 package com.twoyang.prodactivity.server.api;
 
 import lombok.Data;
-import org.springframework.lang.NonNull;
 
 import java.util.List;
 
 @Data
 public class TaskCreation {
-    @NonNull
-    private String color, name;
+
+    private String name;
     private List<Long> categories = List.of();
     private long interval, goal;
 }
